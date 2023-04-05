1e09d0447ecc72a657d4977f4ba7aff6e495a6f0
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-04-05 13:53:41 +0900

    いいねしているユーザーの情報をgetで取得した際、emailはnullで返却するよう変更

--- app/controllers/api/liking_users_controller.rb
+++ app/controllers/api/liking_users_controller.rb
@@ -4,6 +4,7 @@ class Api::LikingUsersController < ApplicationController
   def index
     blog = Blog.find(params[:blog_id])
     @users = blog.liking_users.order(:id)
+    @users.map { |user| user.email = nil }
     my_liking = blog.likings.find_by(user: current_user)
     @destroy_path = my_liking ? api_blog_liking_path(blog, my_liking, format: :json) : nil
   end
