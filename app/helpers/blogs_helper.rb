33b5c4af0ca4ade2f07ae137e5ef7c783f42feec
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-04-17 10:23:49 +0900

    format_contentメソッドの引数をblogに変更する

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module BlogsHelper
-  def format_content(content)
-    simple_format(html_escape(content))
+  def format_content(blog)
+    simple_format(html_escape(blog.content))
   end
 end
